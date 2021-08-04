988d8ba01804044a79ad8265e1a765f5a4f964df
Najada Xara <najada.xara@y42.com>
2021-08-04 08:53:15 +0200

    small fix

--- tap_quickbooks/query_builder.py
+++ tap_quickbooks/query_builder.py
@@ -1,6 +1,6 @@
 def build_query(table_name, bookmark, start_position, max_results, additional_where=None):
     query_base = "SELECT * FROM {}".format(table_name)
-    where_base = " WHERE Metadata.LastUpdatedTime >= '{}'".format(bookmark)
+    where_base = " WHERE Metadata.LastUpdatedTime > '{}'".format(bookmark)
 
     where_clause = where_base
     if additional_where:
